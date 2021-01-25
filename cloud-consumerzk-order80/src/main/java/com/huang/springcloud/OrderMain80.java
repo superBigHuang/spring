1d060222f702659aca93699c56e96db028529328
superBigHuang <841069102@qq.com>
2021-01-25 15:20:41 +0800

    将消费者注册进zookeeper

---
+++ cloud-consumerzk-order80/src/main/java/com/huang/springcloud/OrderMain80.java
@@ -0,0 +1,13 @@
+package com.huang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class, args);
+    }
+}
