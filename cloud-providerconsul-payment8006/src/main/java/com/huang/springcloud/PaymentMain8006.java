1614d2314dfd16be3fd35d69ed9fc5527a6ca8c8
superBigHuang <841069102@qq.com>
2021-01-25 15:51:50 +0800

    将服务提供者注册进consul

---
+++ cloud-providerconsul-payment8006/src/main/java/com/huang/springcloud/PaymentMain8006.java
@@ -0,0 +1,14 @@
+package com.huang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class, args);
+    }
+}
